1dac0dc74ef4c8539de5db6452028c2ff0074626
jgardner78 <72280005+jgardner78@users.noreply.github.com>
2021-02-18 13:14:00 -0800

    Add files via upload
    1st commit

---
+++ stuff.js
@@ -0,0 +1,131 @@
+function Pic11(){
+	pic11.width="150";pic12.width="0";pic13.width="0";
+    pic11.height="200";pic12.height="0";pic13.height="0";
+    pic14.width="0";pic14.height="0";
+	pic11.style.borderWidth="5px";
+	pic12.style.borderWidth="0px";
+    pic13.style.borderWidth="0px";
+    pic14.style.borderWidth="0px";
+	cell[1][1]=1;
+}
+
+function Pic12(){
+	pic11.width="0";pic12.width="150";pic13.width="0";
+    pic11.height="0";pic12.height="200";pic13.height="0";
+    pic14.width="0";pic14.height="0";
+	pic11.style.borderWidth="0px";
+	pic12.style.borderWidth="5px";
+    pic13.style.borderWidth="0px";
+    pic14.style.borderWidth="0px";
+	cell[1][2]=1;
+}
+
+function Pic13(){
+	pic11.width="0";pic12.width="0";pic13.width="150";
+    pic11.height="0";pic12.height="0";pic13.height="200";
+    pic14.width="0";pic14.height="0";
+	pic11.style.borderWidth="0px";
+	pic12.style.borderWidth="0px";
+    pic13.style.borderWidth="5px";
+    pic14.style.borderWidth="0px";
+	cell[1][3]=1;
+}
+
+function Pic14(){
+	pic11.width="0";pic12.width="0";pic13.width="0";
+    pic11.height="0";pic12.height="0";pic13.height="0";
+    pic14.width="150";pic14.height="200";
+	pic11.style.borderWidth="0px";
+	pic12.style.borderWidth="0px";
+    pic13.style.borderWidth="0px";
+    pic14.style.borderWidth="5px";
+	cell[1][4]=1;
+}
+
+function Pic21(){
+	pic21.width="150";pic22.width="0";pic23.width="0";
+    pic21.height="200";pic22.height="0";pic23.height="0";
+    pic24.width="0";pic24.height="0";
+	pic21.style.borderWidth="5px";
+	pic22.style.borderWidth="0px";
+    pic23.style.borderWidth="0px";
+    pic24.style.borderWidth="0px";
+	cell[2][1]=1;
+}
+
+function Pic22(){
+	pic21.width="0";pic22.width="150";pic23.width="0";
+    pic21.height="0";pic22.height="200";pic23.height="0";
+    pic24.width="0";pic24.height="0";
+	pic21.style.borderWidth="0px";
+	pic22.style.borderWidth="5px";
+    pic23.style.borderWidth="0px";
+    pic24.style.borderWidth="0px";
+	cell[2][2]=1;
+}
+
+function Pic23(){
+	pic21.width="0";pic22.width="0";pic23.width="150";
+    pic21.height="0";pic22.height="0";pic23.height="200";
+    pic24.width="0";pic24.height="0";
+	pic21.style.borderWidth="0px";
+	pic22.style.borderWidth="0px";
+    pic23.style.borderWidth="5px";
+    pic24.style.borderWidth="0px";
+	cell[2][3]=1;
+}
+
+function Pic24(){
+	pic21.width="0";pic22.width="0";pic23.width="0";
+    pic21.height="0";pic22.height="0";pic23.height="0";
+    pic24.width="150";pic24.height="200";
+	pic21.style.borderWidth="0px";
+	pic22.style.borderWidth="0px";
+    pic23.style.borderWidth="0px";
+    pic24.style.borderWidth="5px";
+	cell[2][4]=1;
+}
+
+function Pic31(){
+	pic31.width="150";pic32.width="0";pic33.width="0";
+    pic31.height="200";pic32.height="0";pic33.height="0";
+    pic34.width="0";pic34.height="0";
+	pic31.style.borderWidth="5px";
+	pic32.style.borderWidth="0px";
+    pic33.style.borderWidth="0px";
+    pic34.style.borderWidth="0px";
+	cell[3][1]=1;
+}
+
+function Pic32(){
+	pic31.width="0";pic32.width="150";pic33.width="0";
+    pic31.height="0";pic32.height="200";pic33.height="0";
+    pic34.width="0";pic34.height="0";
+	pic31.style.borderWidth="0px";
+	pic32.style.borderWidth="5px";
+    pic33.style.borderWidth="0px";
+    pic34.style.borderWidth="0px";
+	cell[3][2]=1;
+}
+
+function Pic33(){
+	pic31.width="0";pic32.width="0";pic33.width="150";
+    pic31.height="0";pic32.height="0";pic33.height="200";
+    pic34.width="0";pic34.height="0";
+	pic31.style.borderWidth="0px";
+	pic32.style.borderWidth="0px";
+    pic33.style.borderWidth="5px";
+    pic34.style.borderWidth="0px";
+	cell[3][3]=1;
+}
+
+function Pic34(){
+	pic31.width="0";pic32.width="0";pic33.width="0";
+    pic31.height="0";pic32.height="0";pic33.height="0";
+    pic34.width="150";pic34.height="200";
+	pic31.style.borderWidth="0px";
+	pic32.style.borderWidth="0px";
+    pic33.style.borderWidth="0px";
+    pic34.style.borderWidth="5px";
+	cell[3][4]=1;
+}
